5cd92238be5eee8a2f01c8918013aca362703c36
Hassan <hassen.trabelsi@esprit.tn>
2022-11-03 16:04:17 +0100

    update

--- src/main/java/com/esprit/examen/entities/Fournisseur.java
+++ src/main/java/com/esprit/examen/entities/Fournisseur.java
@@ -18,12 +18,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.Builder;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Fournisseur implements Serializable {
 
 	private static final long serialVersionUID = 1L;
